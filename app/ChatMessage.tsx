1030fd81ba67ed0a4a49773fc1b2c53016f26db4
SarahSh <sarah.shkb@gmail.com>
2026-04-23 18:32:19 +0200

    Change handleCopy function so it also works in HTTP (not secure) environment

--- app/ChatMessage.tsx
+++ app/ChatMessage.tsx
@@ -18,8 +18,24 @@ const ChatMessage = ({ msg, i }: ChatMessageType) => {
   const [copied, setCopied] = useState(false);
 
   const handleCopy = async () => {
+    const text = msg.text;
+
     try {
-      await navigator.clipboard.writeText(msg.text);
+      if (navigator.clipboard && window.isSecureContext) {
+        await navigator.clipboard.writeText(text);
+      } else {
+        const textarea = document.createElement("textarea");
+        textarea.value = text;
+        textarea.style.position = "fixed";
+        textarea.style.opacity = "0";
+        document.body.appendChild(textarea);
+        textarea.focus();
+        textarea.select();
+
+        document.execCommand("copy");
+        document.body.removeChild(textarea);
+      }
+
       setCopied(true);
       setTimeout(() => setCopied(false), 1200);
     } catch (err) {
